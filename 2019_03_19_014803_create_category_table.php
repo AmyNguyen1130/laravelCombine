a359272c590e03fad6a33154db00116d9ff249fa
dieuquach <46095714+dieuquach@users.noreply.github.com>
2019-03-19 08:54:45 +0700

    Add files via upload

---
+++ 2019_03_19_014803_create_category_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->String('name',100);
+            $table->String('description');
+            $table->String('image',255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category');
+    }
+}
